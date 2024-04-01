749aa26982ed84b4df89406354d133fa1564df4c
senghong01 <senghonghang@gmail.com>
2024-04-01 17:01:21 +0700

    updated project

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,9 +1,8 @@
 <template>
- <div>
-  <h1>Hello Home Page</h1>
-  <button @click="$router.push('/login')">Next</button>
- </div>
+  <div>
+    <h1>Hello Home Page</h1>
+    <button @click="$router.push('/login')">Next</button>
+  </div>
 </template>
 
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
