88e54ad1d11e5c0e9147dc3c5082a0352df9ca68
senghong <senghong.hang@techbodia.com>
2024-05-09 00:39:49 +0700

    Update table style

--- src/components/admin/QuizDataTable.vue
+++ src/components/admin/QuizDataTable.vue
@@ -2,7 +2,7 @@
   <div>
     <el-table :data="data" border header-cell-class-name="my-table-header" style="width: 100%">
       <el-table-column v-for="(pro, index) in tableProperties" :key="index" :prop="pro[0]" :label="$t(pro[1])"
-        :width="pro[0] === 'Options' ? 'full' : '140'" align="center">
+        :width="pro[0] === 'Options' ? '300' : pro[0] === 'CreatedAt' ? '200' : ''" align="center">
         <template #default="{ row }">
           <div v-if="pro[0] === 'Options'">
             <ul>
@@ -12,9 +12,12 @@
           <div v-if="pro[0] === 'CorrectAnswer'">
             <span class="text-text3 font-medium">{{ row.CorrectAnswer }}</span>
           </div>
+          <div v-if="pro[0] === 'CreatedAt'">
+            <span>{{ dateTimeConverter(row.CreatedAt) }}</span>
+          </div>
         </template>
       </el-table-column>
-      <el-table-column fixed="right" label="Operations" align="center" width="120">
+      <el-table-column fixed="right" :label="$t('actions')" align="center" width="100">
         <template #default="scope">
           <el-button @click="onUpdate(scope.row.QuizId)" type="warning" link :icon="Edit" circle />
         </template>
@@ -27,10 +30,11 @@
 import { defineProps } from 'vue';
 import { Edit } from '@element-plus/icons-vue';
 import type { TableProperty } from '@/models/tableProperty';
+import { dateTimeConverter } from '@/libraries/utils/converter'
 
 defineProps<{
   tableProperties: TableProperty,
-  data:Object,
+  data: Object,
   onUpdate: Function,
 }>();
 </script>
