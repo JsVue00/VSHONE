f41649ec1d4d6e12959f6d98022d3fa7d2da2f58
senghong01 <senghonghang@gmail.com>
2024-05-12 02:58:03 +0700

    created confirm dialog component

---
+++ src/components/admin/ConfirmDialog.vue
@@ -0,0 +1,24 @@
+<template>
+    <el-dialog :model-value="isDialogVisible" :title="title" width="500" @close="$emit('close')">
+        <span>{{ content }}</span>
+        <template #footer>
+            <div class="dialog-footer">
+                <el-button type="primary" :loading="isLoading" @click="confirm">
+                    {{ $t('confirm') }}
+                </el-button>
+            </div>
+        </template>
+    </el-dialog>
+</template>
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+defineProps<{
+    isDialogVisible: boolean,
+    title: string,
+    content?: string,
+    confirm: Function,
+    isLoading?: boolean,
+}>();
+
+</script>
