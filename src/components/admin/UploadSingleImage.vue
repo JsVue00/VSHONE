b73c6281db0db9effbcf7a7e78a6c89e05930401
senghong01 <senghonghang@gmail.com>
2024-05-26 11:25:02 +0700

    [Component] Modify image display

--- src/components/admin/UploadSingleImage.vue
+++ src/components/admin/UploadSingleImage.vue
@@ -1,18 +1,9 @@
 <template>
   <div class="flex w-full justify-between">
     <div class="max-w-[200px] max-h-[100px] rounded-md overflow-hidden border">
-      <img
-        v-if="props.FileName !== ''"
-        class="h-full w-full object-contain"
-        :src="getImage(props.Folder, props.FileName)"
-        alt=""
-      />
-      <img
-        v-else
-        class="h-full w-full object-contain"
-        src="../../assets/images/noImage.jpg"
-        alt=""
-      />
+      <img v-if="props.FileName !== ''" class="h-full w-full object-contain"
+        :src="getImage(props.Folder, props.FileName)" alt="" />
+      <img v-else class="h-full w-full object-contain" src="../../assets/images/noImage.jpg" alt="" />
     </div>
     <div>
       <el-upload style="width: 100%" :auto-upload="false" :on-change="handleChange">
@@ -26,6 +17,7 @@
 <script lang="ts" setup>
 import { getImage } from '@/apis/api';
 
+
 const props = defineProps({
   FileName: {
     type: String,
@@ -37,4 +29,6 @@ const props = defineProps({
   },
   handleChange: Function
 });
+
+
 </script>
