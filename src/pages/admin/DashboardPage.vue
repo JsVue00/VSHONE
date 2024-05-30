ffd2aef1a763d8c73a147b642cd84c5ecdeba417
senghong01 <senghonghang@gmail.com>
2024-05-30 22:51:56 +0700

    [Admin] Completed Dashboard UI

--- src/pages/admin/DashboardPage.vue
+++ src/pages/admin/DashboardPage.vue
@@ -1,5 +1,34 @@
 <template>
-    <div>
-        Dash borad
+    <div class="flex flex-wrap gap-4">
+        <div v-for="(data, index) in fakeDashboardData" :key="index"
+            class="text-text2 dashboard-card  w-52 hover:w  border-primary rounded-md bg-white content-center p-4 ">
+            <div class="text-2xl font-medium">{{ data.total }}</div>
+            <div>{{ data.name }}</div>
+
+        </div>
     </div>
 </template>
+<script lang="ts" setup>
+import { ref } from "vue";
+interface FakeDashboardData {
+    name: string;
+    total: number;
+}
+
+const fakeDashboardData = ref<FakeDashboardData[]>([
+    { name: 'total video', total: 100 },
+    { name: 'total quizzes', total: 200 },
+    { name: 'total cities', total: 300 },
+    { name: 'total provinces', total: 300 },
+    { name: 'total tour area', total: 300 },
+]);
+</script>
+
+<style scoped>
+.dashboard-card:hover {
+    transition: all 0.5s ease-in-out;
+    background: var(--primary);
+    color: var(--text1);
+    box-shadow: 4px 4px 10px #4a739b;
+}
+</style>
