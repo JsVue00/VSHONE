4dfb9cb3182c41ef1b17874a53b1a34727996fba
senghong01 <senghonghang@gmail.com>
2024-05-30 23:40:18 +0700

    [Admin] Modified dashboard title style

--- src/pages/admin/DashboardPage.vue
+++ src/pages/admin/DashboardPage.vue
@@ -1,4 +1,9 @@
 <template>
+    <div class="text-xl text-text2   mb-4 border-b-[1px] border-gray-600 flex w-full ">
+        <div class="w-full flex overflow-hidden">
+            <div class="title">{{ $t('dashboard') }}</div>
+        </div>
+    </div>
     <div class="flex flex-wrap gap-4">
         <div v-for="(data, index) in fakeDashboardData" :key="index"
             class="text-text2 dashboard-card  w-52 hover:w  border-primary rounded-md bg-white content-center p-4 ">
@@ -30,5 +35,23 @@ const fakeDashboardData = ref<FakeDashboardData[]>([
     background: var(--primary);
     color: var(--text1);
     box-shadow: 4px 4px 10px #4a739b;
+}
+
+.title {
+    z-index: 2;
+    color: var(--text1);
+    padding: 4px 8px;
+    background: var(--background1);
+    position: relative;
+}
+
+.title::after {
+    content: '';
+    top: 0;
+    right: -30px;
+    border-top: 100px solid var(--background1);
+    border-right: 60px solid transparent;
+    position: absolute;
+    z-index: -1;
 }
 </style>
